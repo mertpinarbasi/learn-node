6bdb122eb665e040d6c9f89646f9f9556364741c
mertpinarbasi <mpinarbasi35@gmail.com>
2022-01-28 18:26:08 +0300

    GET and POST methods has been added

--- 01-basic-http-server/server.js
+++ 01-basic-http-server/server.js
@@ -1,23 +1,69 @@
-const http = require('http');
+const {
+    stat
+} = require("fs");
+const http = require("http");
 const todos = [{
         id: 1,
-        text: 'Todo One'
+        text: "Todo One",
     },
     {
         id: 2,
-        text: 'Todo two'
-    }
-]
+        text: "Todo two",
+    },
+];
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('X-Powered-By', 'Node.js');
+    const {
+        method,
+        url
+    } = req;
 
-    res.end(JSON.stringify({
-        success: true,
-        data: todos
-    }));
+    let body = [];
+    req
+        .on("data", (chunk) => {
+            body.push(chunk);
+        })
+        .on("end", () => {
+            body = Buffer.concat(body).toString();
+            let status = 404;
+            const response = {
+                success: false,
+                data: null,
+            };
+            if (method == 'GET' && url == '/todos') {
+                status == 200;
+                response.success = true;
+                response.data = todos;
+            } else if (method == 'POST' && url == '/todos') {
+                const {
+                    id,
+                    text
+                } = JSON.parse(body);
+                todos.push({
+                    id,
+                    text
+                })
+                status = 201;
+                response.success = true;
+                response.data = todos;
+            }
+            res.writeHead(200, {
+                // Decides to type of the data
+                "Content-Type": "application/json",
+                // X-Powered-By : information about the server side language
+                "X-Powered-By": "Node.js",
+            });
+
+
+            res.end(
+                JSON.stringify({
+                    response,
+                })
+            );
+        });
+
+
 });
-const PORT = 32000;
+const PORT = 1111;
 server.listen(PORT, () => {
-    console.log("works")
-})
+    console.log("works");
+});
